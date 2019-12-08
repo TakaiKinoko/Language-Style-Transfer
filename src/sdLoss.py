f05eb016ead52b92aa4cb93076131b9d2fe65fe6
Fang Han <hanfangviolin@gmail.com>
2019-12-08 15:08:55 -0500

    added style discrepancy loss in sdLoss.py

--- src/sdLoss.py
+++ src/sdLoss.py
@@ -18,19 +18,11 @@ def getTargetStyle(dim = 500, seed = 10):
 
     return y_target
 
-def styleDiscrepancy(ys, y_target):
-    '''
-    compute the style discrepancy between two style embeddings
-    :param ys: output vector of the style encoder, which is the style embedding of source data
-    :param y_target: style representation of the target domain
-    :return: d(ys, y_target) = l2-norm(ys - y_target)
-    '''
-    return np.linalg.norm(np.subtract(ys, y_target))
-
 def sdLoss(p_ds, y_target, Ys):
     '''
     computes the style discrepancy loss
     :param p_ds: an array of probabilities, output from discriminator D_s, denoting the possibility that a source sentence has the target style
+    :param y_target: style representation for the target domain
     :param Ys: an array of style embeddings for each sentence in Xs, output from Ey (style encoder)
     '''
     dis = [] # list of style discrepencies
@@ -39,3 +31,12 @@ def sdLoss(p_ds, y_target, Ys):
         dis.append(p * pow(styleDiscrepancy(ys, y_target), 2))
     
     return np.mean(dis)
+
+def styleDiscrepancy(ys, y_target):
+    '''
+    compute the style discrepancy between two style embeddings
+    :param ys: output vector of the style encoder, which is the style embedding of source data
+    :param y_target: style representation of the target domain
+    :return: d(ys, y_target) = l2-norm(ys - y_target)
+    '''
+    return np.linalg.norm(np.subtract(ys, y_target))
